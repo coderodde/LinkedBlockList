7ef97ec5676ed3ab04d0caf2dea20ef1074bef8c
coderodde <rodion.efremov@helsinki.fi>
2018-08-23 15:23:20 +0300

    ...

--- src/net/coderodde/util/experimental/LinkedBlockList.java
+++ src/net/coderodde/util/experimental/LinkedBlockList.java
@@ -11,7 +11,7 @@ public final class LinkedBlockList<T> {
 
     private static final int DEFAULT_BLOCK_CAPACITY = 64;
     private static final int MINIMUM_BLOCK_CAPACITY = 4;
-    
+
     /**
      * This static inner class implements the actual blocks storing the 
      * elements.
@@ -19,94 +19,77 @@ public final class LinkedBlockList<T> {
      * @param <T> the element type.
      */
     private static final class Block<T>  {
-        
+
         /**
          * The length of {@code array}.
          */
         final int capacity;
-        
+
+        /**
+         * The mask used for modulo computation.
+         */
         final int indexMask;
-        
+
         /**
          * The number of elements in this block.
          */
         int size;
-        
+
         /**
          * The index of the very first element in this block.
          */
         int headIndex;
-        
+
         /**
          * The array holding all the elements belonging to this block.
          */
         T[] array;
-        
+
         /**
          * The previous block.
          */
         Block<T> previousBlock;
-        
+
         /**
          * The next block.
          */
         Block<T> nextBlock;
-        
+
         Block(int capacity) {
             this.capacity = capacity;
             this.indexMask = capacity - 1;
             this.array = (T[]) new Object[capacity];
         }
-        
+
         T get(int logicalIndex) {
             return array[(headIndex + logicalIndex) & indexMask];
         }
-        
+
         void setNull(int logicalIndex) {
             array[(headIndex + logicalIndex) & indexMask] = null;
         }
-        
-        // Shifts 'elementsToShift' elements starting from logical index
-        // 'startIndex' one position to the left.
-        void shiftToLeft(int startIndex, int elementsToShift) {
-            for (int elementsShifted = 0;
-                     elementsShifted < elementsToShift;
-                     elementsShifted++) {
-                array[(startIndex + elementsShifted - 1) & indexMask] =
-                        array[(startIndex + elementsShifted) & indexMask];
-            }
-        }
-        
-        void shiftToRight(int startIndex, int elementsToShift) {
-            for (int elementsShifted = 0;
-                     elementsShifted < elementsToShift;
-                     elementsShifted++) {
-                array[(startIndex + elementsShifted) & indexMask] =
-                        array[(startIndex + elementsShifted - 1) & indexMask];
-            }
-        }
     }
-    
+
     /**
      * The number of elements in this list.
      */
     private int size;
-    
+
     /**
      * The number of blocks contained by this list.
      */
     private int blocks;
-    
+
     /**
      * The first block in the chain.
      */
     private Block<T> headBlock;
-    
+
     /**
      * The last block in the chain.
      */
     private Block<T> tailBlock;
-    
+
     /**
      * The block capacity.
      */
@@ -115,21 +98,21 @@ public final class LinkedBlockList<T> {
      * The mask used for index computation.
      */
     private int indexMask;
-    
+
     public LinkedBlockList(int blockCapacity) {
         blockCapacity = Math.max(blockCapacity, MINIMUM_BLOCK_CAPACITY);
         blockCapacity = ceilToPowerOfTwo(blockCapacity);
         this.blockCapacity = blockCapacity;
         this.indexMask = blockCapacity - 1;
     }
-    
+
     public LinkedBlockList() {
         this(DEFAULT_BLOCK_CAPACITY);
     }
-    
+
     public void add(int index, T element) {
         checkAddIndex(index);
-        
+
         if (size == 0) {
             headBlock = new Block<>(blockCapacity);
             tailBlock = headBlock;
@@ -138,20 +121,20 @@ public final class LinkedBlockList<T> {
             size = 1;
             return;
         }
-        
+
         Block<T> block = headBlock;
-        
+
         while (index > block.size) {
             index -= block.size;
             block = block.nextBlock;
         }
-        
+
         if (block.size == block.capacity) {
             // Create a new block and move to it as little elements as possible:
             int elementsOnLeft = index;
             int elementsOnRight = block.size - index;
             Block<T> newBlock = new Block<>(blockCapacity);
-            
+
             if (elementsOnLeft < elementsOnRight) {
                 // Add newBlock before block and move to it the prefix of the
                 // current block and append the new element:
@@ -161,7 +144,7 @@ public final class LinkedBlockList<T> {
                     newBlock.array[newBlockIndex] = block.get(newBlockIndex);
                     block.setNull(newBlockIndex);
                 }
-                
+
                 newBlock.array[elementsOnLeft] = element;
                 newBlock.size = elementsOnLeft + 1;
                 newBlock.nextBlock = block;
@@ -170,7 +153,7 @@ public final class LinkedBlockList<T> {
                 block.size -= elementsOnLeft;
                 block.headIndex = 
                         (block.headIndex + elementsOnLeft) & indexMask;
-                
+
                 if (newBlock.previousBlock == null) {
                     headBlock = newBlock;
                 } else {
@@ -180,7 +163,7 @@ public final class LinkedBlockList<T> {
                 block.size -= elementsOnRight;
                 newBlock.array[0] = element;
                 int targetIndex = 1;
-                
+
                 for (int newBlockIndex = index; 
                          newBlockIndex < elementsOnRight;
                          newBlockIndex++) {
@@ -188,12 +171,12 @@ public final class LinkedBlockList<T> {
                     block.setNull(newBlockIndex);
                     targetIndex++;
                 }
-                
+
                 newBlock.size = elementsOnRight + 1;
                 newBlock.previousBlock = block;
                 newBlock.nextBlock = block.nextBlock;
                 block.nextBlock = newBlock;
-                
+
                 if (newBlock.nextBlock == null) {
                     tailBlock = newBlock;
                 } else {
@@ -204,7 +187,7 @@ public final class LinkedBlockList<T> {
             // The current block is not full so insert into it:
             int elementsOnLeft = index;
             int elementsOnRight = block.size - index;
-            
+
             if (elementsOnLeft < elementsOnRight) {
                 // Shift the leftmost elements one position to the left:
                 for (int elementIndex = 0; 
@@ -213,14 +196,14 @@ public final class LinkedBlockList<T> {
                     int sourceIndex = 
                             (block.headIndex + elementIndex) 
                             & indexMask;
-                    
+
                     int targetIndex = 
                             (block.headIndex + elementIndex - 1) 
                             & indexMask;
-                    
+
                     block.array[targetIndex] = block.array[sourceIndex];
                 }
-                
+
                 block.array[(block.headIndex + index - 1) & indexMask] =
                         element;
                 block.headIndex = (block.headIndex - 1) & indexMask;
@@ -233,59 +216,59 @@ public final class LinkedBlockList<T> {
                     int sourceIndex = 
                             (block.headIndex + block.size - elementIndex - 1) 
                             & indexMask;
-                    
+
                     int targetIndex = 
                             (block.headIndex + block.size - elementIndex) 
                             & indexMask;
-                    
+
                     block.array[targetIndex] = block.array[sourceIndex];
                 }
-                
+
                 block.array[(block.headIndex + index) & indexMask] = element;
                 block.size++;
             }
         }
-        
+
         size++;
     }
-    
+
     public T get(int index) {
         checkAccessIndex(index);
         Block<T> block = headBlock;
-        
+
         while (index >= block.size) {
             index -= block.size;
             block = block.nextBlock;
         }
-        
+
         return block.get(index);
     }
-    
+
     public void remove(int index) {
         checkAccessIndex(index);
         Block<T> targetBlock = headBlock;
-        
+
         while (index >= targetBlock.size) {
             index -= targetBlock.size;
             targetBlock = targetBlock.nextBlock;
         }
-        
+
         if (targetBlock.size == 1) {
             // The target block contains only one element. Unlink it from the
             // chain of blocks:
             if (targetBlock == headBlock) {
                 headBlock = headBlock.nextBlock;
-                
+
                 if (headBlock != null) {
                     headBlock.previousBlock = null;                    
                 }
             } else {
                 targetBlock.previousBlock.nextBlock = targetBlock.nextBlock;
             }
-            
+
             if (targetBlock == tailBlock) {
                 tailBlock = tailBlock.previousBlock;
-                
+
                 if (tailBlock != null) {
                     tailBlock.nextBlock = null;
                 }
@@ -295,7 +278,7 @@ public final class LinkedBlockList<T> {
         } else {
             int elementsOnLeft = index;
             int elementsOnRight = targetBlock.size - index - 1;
-            
+
             if (elementsOnLeft < elementsOnRight) {
                 // Shift the leftmost elements in the target block one position
                 // to the right:
@@ -306,7 +289,7 @@ public final class LinkedBlockList<T> {
                     targetBlock.array[targetIndex] = 
                             targetBlock.array[sourceIndex];
                 }
-                
+
                 targetBlock.setNull(0);
                 targetBlock.headIndex = (targetBlock.headIndex + 1) & indexMask;
                 targetBlock.size--;
@@ -320,19 +303,19 @@ public final class LinkedBlockList<T> {
                     targetBlock.array[targetIndex] = 
                             targetBlock.array[sourceIndex];
                 }
-                
+
                 targetBlock.size--;
                 targetBlock.setNull(targetBlock.size);
             }
         }
-        
+
         size--;
     }
-    
+
     public int size() {
         return size;
     }
-    
+
     /**
      * Returns a number between zero and one indicating how densely the blocks
      * are.
@@ -342,36 +325,36 @@ public final class LinkedBlockList<T> {
     public float getDensityFactor() {
         return ((float) size) / blocks * blockCapacity;
     }
-    
+
     private void checkAccessIndex(int index) {
         if (index < 0) {
             throw new IndexOutOfBoundsException("index(" + index + ") < 0");
         }
-        
+
         if (index >= size) {
             throw new IndexOutOfBoundsException(
                     "index(" + index + ") >= (" + size + ")");
         }
     }
-    
+
     private void checkAddIndex(int index) {
         if (index < 0) {
             throw new IndexOutOfBoundsException("index(" + index + ") < 0");
         }
-        
+
         if (index > size) {
             throw new IndexOutOfBoundsException(
                     "index(" + index + ") > (" + size + ")");
         }
     }
-    
+
     private static int ceilToPowerOfTwo(int number) {
         int ret = 1;
-        
+
         while (ret < number) {
             ret <<= 1;
         }
-        
+
         return ret;
     }
 }
